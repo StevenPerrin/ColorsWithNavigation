92277a3afb30e4bcccbbe2b08cbea07b153be1f3
Steven Perrin <spcnn@mail.missouri.edu>
2019-04-04 15:03:39 -0500

    added color struct and changed background color on cells

---
+++ ColorsWithNavigation/ColorsWithNavigation/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by Steven Perrin on 4/4/19.
+//  Copyright © 2019 Steven Perrin. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    let name: String
+    let uiColor: UIColor
+}
+
+
