b9d3ca082cac76a4bb8f99c69704f6550350c5ea
Steven Perrin <spcnn@mail.missouri.edu>
2019-04-04 16:35:37 -0500

    added detailed view controller and navigation controllers

---
+++ ColorsWithNavigation/ColorsWithNavigation/ColorDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ColorDetailViewController.swift
+//  ColorsWithNavigation
+//
+//  Created by Steven Perrin on 4/4/19.
+//  Copyright © 2019 Steven Perrin. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+
+    var color: Color?
+    
+    
+    @IBOutlet weak var colorNameLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorNameLable.text = color?.name
+        self.view.backgroundColor = color?.uiColor
+        self.title = color?.name
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
